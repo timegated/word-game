1bcc97c44603f6e855bee88e6da76806e03f7b34
dbeccaria826 <beccdaniel@gmail.com>
2020-05-02 11:01:40 -0400

    set up constructors for the game itself and the ui elements, using fetch to grab words from json file

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,162 +1,48 @@
-(function init() {
-    const wordGame = {
-        jsConcepts: [{
-                word: 'Closures',
-                link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures'
-            },
+(function () {
+    // Game Constructor
+
+    function WordGame() {
+        this.totalGuesses = 10;
+        this.wins = 0;
+        this.losses = 0;
+    };
+
+    function Elements() {
+        this.elements = [
             {
-                word: 'Prototype',
-                link: 'https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes'
-            },
+                randomWord: document.querySelector('.random-word')
+            }, 
             {
-                word: 'Scope',
-                link: 'https://developer.mozilla.org/en-US/docs/Glossary/Scope'
+                randomLink: document.querySelector('.random-link')
             },
-            {
-                word: 'Object',
-                link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object'
-            },
-            {
-                word: 'Function',
-                link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function'
-            },
-            {
-                word: 'HTTP',
-                link: 'https://developer.mozilla.org/en-US/docs/Web/HTTP'
-            },
-            {
-                word: 'REST',
-                link: 'https://developer.mozilla.org/en-US/docs/Glossary/REST'
-            },
-            {
-                word: 'NODEJS',
-                link: 'https://developer.mozilla.org/en-US/docs/Glossary/Node.js'
-            },
-            {
-                word: 'IFFE',
-                link: 'https://developer.mozilla.org/en-US/docs/Glossary/IIFE'
-            }
-        ],
-        correctWord: [],
-        correctGuesses: [],
-        userGuess: new Set(),
-        targets: {
-            losses: document.querySelector('.losses'),
-            wins: document.querySelector('.wins'),
-            randomWord: document.querySelector('.random-word'),
-            guesses: document.querySelector('.guesses'),
-            remainingGuesses: document.querySelector('.remaining-guess')
-        }
+        ]
     };
 
-    Object.prototype.randomIndex = function () {
-        let randomIndex = Math.floor(Math.random() * this.jsConcepts.length)
+    // Modifiying prototype of game constructor
 
-        function saveIndex() {
-            return randomIndex;
-        }
-        return saveIndex();
+    WordGame.prototype.getRandomWords = function (arr) {
+        return arr[Math.floor(Math.random() * arr.length) + 1].word;
     };
 
-    Object.prototype.randomWord = function () {
-        return this.jsConcepts[this.randomIndex()];
+    WordGame.prototype.getWordsFromJson = function (url) {
+        return fetch(url)
+            .then(function (res) {
+                return res.json();
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
     };
 
+    // Instantiate a new game
 
-    let {
-        targets,
-        correctWord,
-        correctGuesses,
-        userGuess
-    } = wordGame;
-    const {
-        remainingGuesses,
-        losses,
-        wins,
-        randomWord,
-        guesses
-    } = targets;
-    let correctLength = correctGuesses.length;
-    remainingGuesses.textContent = 10;
-    wins.textContent = 0;
-    losses.textContent = 0;
+    const newGame = new WordGame();
 
-    const createTheBlanks = () => {
-        const randomWord = wordGame.randomWord();
-        const word = [...randomWord.word];
-
-        word.forEach(letter => {
-            correctGuesses.push('_')
-            correctWord.push(letter.toLowerCase())
-        });
-
-    };
-
-    const displayTheBlanks = () => {
-        randomWord.textContent = correctGuesses.join(' ');
-        return true
-    };
-
-
-    const logCorrectGuess = (letter, func) => {
-
-        for (let i = 0; i < wordGame.correctWord.length; i++) {
-            if (letter === wordGame.correctWord[i]) {
-                correctGuesses[i] = letter.toLowerCase();
-            };
-        };
-        if (func()) {
-            correctLength++
-            console.log(correctLength)
-            console.log(correctWord.length)
-            if (correctLength === correctWord.length) {
-                init()
-            };
-        };
-    };
-
-    const displayUserGuesses = (letter) => {
-        guesses.innerHTML = `<p>${[...userGuess.add(letter)]}<p>`;
-
-        if (remainingGuesses.textContent == 0) {
-            losses.textContent++;
-            initialize();
-        };
-    };
-
-    const hideMessage = () => {
-        const messageElement = document.querySelector('.message');
-        messageElement.classList.add('play-state');
-    };
-
-
-    const initialize = () => {
-        remainingGuesses.textContent = 10;
-        randomWord.textContent = '';
-        guesses.textContent = '';
-        userGuess.clear();
-        correctWord = [];
-        correctGuess = [];
-        createTheBlanks();
-    };
-
-    document.addEventListener('DOMContentLoaded', () => {
-        createTheBlanks();
-        displayTheBlanks();
-        console.log(correctWord.length)
-        console.log(correctWord)
-        document.addEventListener('keyup', (e) => {
-            if (e.keyCode >= 65 && e.keyCode <= 90) {
-                hideMessage()
-                if (correctWord.indexOf(e.key) > -1) {
-                    logCorrectGuess(e.key, displayTheBlanks)
-                } else {
-                    displayUserGuesses(e.key);
-                    remainingGuesses.textContent--;
-                };
-                console.log(e.key);
-            };
-        });
+    newGame.getWordsFromJson('/wordData.json').then(function (res) {
+        const uiElements = new Elements();
+        const { elements } = uiElements;
+        const [randomWord, randomLink] = elements;
+        newGame.words = res.words;
+        randomWord.randomWord.textContent = newGame.getRandomWords(newGame.words);
     });
-
-}());
+})();
